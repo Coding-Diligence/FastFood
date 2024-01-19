1bae51c940e2b9685b0517ca2de73b024ac569cb
Coding-Diligence <kilian.poussard.pro@gmail.com>
2024-01-19 14:16:59 +0100

    0.3

--- index.php
+++ index.php
@@ -4,12 +4,4 @@
 <?php
 require_once 'templates/header.php';
 require_once 'config/system.php';
-try {
-    $bdLink = new PDO('mysql:host=' . $paramsServer['server'] . ';port=' . $paramsServer['port'] . ';dbname=' . $paramsServer['database'] . ';charset=utf8', $paramsServer['username'], $paramsServer['password']);
-    $bdLink->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    $bdLink->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    print 'Connection done';
-} catch (PDOException $e) {
-    print "Error ! database connection error<br/>";
-    die();
-}
+require_once 'require/common.php';
--- require/common.php
+++ require/common.php
@@ -1,2 +1,10 @@
 <?php 
-return true;
+try {
+    $bdLink = new PDO('mysql:host=' . $paramsServer['server'] . ';port=' . $paramsServer['port'] . ';dbname=' . $paramsServer['database'] . ';charset=utf8', $paramsServer['username'], $paramsServer['password']);
+    $bdLink->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $bdLink->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    print 'Connection done';
+} catch (PDOException $e) {
+    print "Error ! database connection error<br/>";
+    die();
+}
